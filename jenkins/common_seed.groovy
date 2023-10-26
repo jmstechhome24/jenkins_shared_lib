2fd002b71aa204b2e068a039404ff333ee6d9879
jmstechhome24 <139795195+jmstechhome24@users.noreply.github.com>
2023-10-26 21:42:11 +0530

    Update common_seed.groovy

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -1,4 +1,4 @@
-def baseUrl = "https://github.com/ybmadhu/"
+def baseUrl = "https://github.com/jmstechhome24/"
 def repoName = "$reponame"
 def gitRepoUrl= baseUrl + repoName + '.git'
 def jobName = "$reponame"
@@ -22,7 +22,7 @@ def jobName = "$reponame"
                     git {
                         remote {
                             url(gitRepoUrl)
-                            credentials('git_credentials')
+                            credentials('github_account_24')
                         }
                         branches('master')
                         extensions {
